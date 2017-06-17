d8a90c88058792064abc83b6db553fb46a40ea43
Indra Firmansyah <indra@entersocio.com>
2017-06-17 21:50:11 +0700

    Added example of send mail with queue
    * using redis for driver
    * using mailgun for mail driver

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\SendEmail;
+use Illuminate\Support\Facades\Mail;
+
+// Model
+use App\Email;
 
 class EmailController extends Controller
 {
-    //
+    public function send(Request $request)
+    {
+        $emails = Email::all();
+        foreach ($emails as $key => $value) {
+            Mail::to($value)->queue(new SendEmail());
+        }
+        return response('Email queued!');
+    }
 }
